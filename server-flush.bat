c4a92bdfc5c31aa6fb6723da530a0a2c85fd0301
githubmahan <sebs3054@naver.com>
2024-12-01 10:55:38 +0900

    레포지토리 업로드

---
+++ server-flush.bat
@@ -0,0 +1,5 @@
+del "Server\run.bat"
+
+rmdir /S /Q "./Server/node_modules"
+rmdir /S /Q "./Server/lib/node_modules"
+rmdir /S /Q "./Server/lib/etc"
---
+++ server-setup.bat
@@ -0,0 +1,6 @@
+cd ./Server
+node setup
+cd ./lib
+npm install discord.js@11
+echo npm start > ../run.bat
+grunt default pack
