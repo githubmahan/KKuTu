40f479b7298070ff93efba26b76185ab8ebdfc70
githubmahan <sebs3054@naver.com>
2025-10-23 09:26:37 +0900

    일부 파일 수정 및 제거

--- server-setup.bat
+++ server-setup.bat
@@ -1,10 +1,5 @@
 cd ./Server
 node setup
 cd ./lib
-npm install axios
-npm install pg@6.1.2
-npm install ws@3.3.1
-npm install passport-roblox
-npm install cors
 echo npm start > ../run.bat
 npm run build
